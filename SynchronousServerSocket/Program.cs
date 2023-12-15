838179884778862ce98c868a9e82ade996b28c69
Ümmügülüm <ummugulsumzengin1@gmail.com>
2023-12-15 22:28:21 +0300

    Add project files.

---
+++ SynchronousServerSocket/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynchronousServerSocket
+{
+	internal class Program
+	{
+		public class SyncServerSocket
+		{
+			public static string data = null;
+			public static void StartListener()
+			{
+				byte[] bytes = new byte[1024];
+
+				IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+				IPAddress ipAddress = ipHost.AddressList[0];
+				IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+				Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+				try
+				{
+					listener.Bind(localEndPoint);
+					listener.Listen(10);
+					while (true)
+					{
+						Console.WriteLine($"waiting for a connection ...");
+						Socket handler = listener.Accept();
+						data = null;
+						while (true)
+						{
+							int byteRec = handler.Receive(bytes);
+							data += Encoding.ASCII.GetString(bytes, 0, byteRec);
+							if (data.IndexOf("<EQF>") > -1)
+							{
+								break;
+							}
+						}
+
+						Console.WriteLine("Test recived : {data}");
+						byte[] msg = Encoding.ASCII.GetBytes(data);
+						handler.Send(msg);
+						handler.Shutdown(SocketShutdown.Both);
+						handler.Close();
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					throw;
+				}
+
+				Console.WriteLine("pess any to exit program");
+				Console.ReadLine();
+			}
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("enter to continue ");
+			Console.ReadLine();
+			SyncServerSocket.StartListener();
+			Console.ReadLine();
+		}
+
+
+	}
+}
---
+++ SynchronousSockets/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynchronousSockets
+{
+	internal class Program
+	{
+		public class SyncSocketClient
+		{
+			public static void StartClient()
+			{
+				byte[] bytes = new byte[1024];
+				try
+				{
+					var hostName = Dns.GetHostName();
+					IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+					Console.WriteLine($"Host: {hostName}");
+					IPAddress ip = ipHost.AddressList[0];
+					IPEndPoint remoteIP = new IPEndPoint(ip, 5000);
+
+					Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+					try
+					{
+						sender.Connect(remoteIP);
+						Console.WriteLine("socket connected");
+						sender.RemoteEndPoint.ToString();
+						byte[] msg = Encoding.ASCII.GetBytes("this is just a test");
+						int byteSent = sender.Send(msg);
+						int byteRec = sender.Receive(bytes);
+						Console.WriteLine($"Echoned test {Encoding.ASCII.GetString(bytes, 0, byteRec)}");
+
+						//release the socket
+						sender.Shutdown(SocketShutdown.Both);
+						sender.Close();
+					}
+					catch (ArgumentNullException e)
+					{
+						Console.WriteLine(e.Message);
+						throw;
+					}
+					catch (SocketException e)
+					{
+						Console.WriteLine(e.Message);
+
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.Message);
+
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					throw;
+				}
+			}
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("press any key to  cont....");
+			Console.ReadLine();
+			SyncSocketClient.StartClient();
+			Console.ReadLine();
+		}
+
+
+	}
+}
